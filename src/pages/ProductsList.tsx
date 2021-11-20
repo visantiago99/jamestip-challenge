93f76c7c4df369433d81d0b683019b70f21e57b9
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-11-20 11:12:13 -0300

    fix context

--- src/pages/ProductsList.tsx
+++ src/pages/ProductsList.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ProductsContext from '../context/ProductsContext';
 import { Link } from 'react-router-dom';
 
+
 export default function ProductsList() {
+  const { productList } = useContext(ProductsContext)
+  
+  
+  
   return (
     <div>
-      Product List
+      <ul>
+        {productList.map((pdt) => <li>
+          <p>{pdt.productId}</p>
+          <p>{pdt.category}</p>
+          <p>{pdt.name}</p>
+          <p>{pdt.suplierName}</p>
+          <p>{pdt.price}</p>
+        </li>)}
+      </ul>
+      <br />
+      <Link to="/">
+        <button type="button">Voltar</button>
+      </Link>
     </div>
   )
 }
