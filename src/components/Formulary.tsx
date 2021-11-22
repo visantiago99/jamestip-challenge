1260eaab8333afc961d0f7042e7c3ebe3a15ee07
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-11-21 21:31:49 -0300

    Fix localstorage problems

--- src/components/Formulary.tsx
+++ src/components/Formulary.tsx
@@ -2,6 +2,54 @@ import React, { useContext, useState } from 'react';
 import ProductsContext from '../context/ProductsContext';
 import { productsItf } from '../interfaces/ProductInterface';
 
+// module StorageHelper {
+//   export interface IStorageItem {
+//       key: string;
+//       value: any;
+//   }
+
+//   export class StorageItem {
+//       key: string;
+//       value: any;
+
+//       constructor(data: IStorageItem) {
+//           this.key = data.key;
+//           this.value = data.value;
+//       }
+//   }
+
+//   // class for working with local storage in browser (common that can use other classes for store some data)
+//   export class LocalStorageWorker {
+//       localStorageSupported: boolean;
+
+//       constructor() {
+//           this.localStorageSupported = typeof window['localStorage'] != "undefined" && window['localStorage'] != null;
+//       }
+
+//       // add value to storage
+//       add(key: string, item: string) {
+//           if (this.localStorageSupported) {
+//               localStorage.setItem(key, item);
+//           }
+//       }
+
+//       // get all values from storage (all items)
+//       getAllItems(): Array<StorageItem> {
+//           var list = new Array<StorageItem>();
+
+//           for (var i = 0; i < localStorage.length; i++) {
+//               var key = localStorage.key(i);
+//               var value = localStorage.getItem(key);
+
+//               list.push(new StorageItem({
+//                   key: key,
+//                   value: value
+//               }));
+//           }
+
+//           return list;
+//       }
+
 export default function Formulary() {
   const [input, setInput] = useState<productsItf>({
   productId: "",
@@ -21,9 +69,13 @@ export default function Formulary() {
   }
 
   const setLocalStorage = () => {
-    if (!localStorage.productList) {
-      localStorage.setItem('productList', JSON.stringify(productList))
+    console.log(productList.length >= 1)
+    if (!localStorage.productList && input) {
+      localStorage.setItem('productList', JSON.stringify([input]))
     }
+    const localObj = JSON.parse(localStorage.getItem('productList') || "")
+    let result = [...localObj, ...productList]
+    localStorage.setItem('productList', JSON.stringify(result))
   }
 
   const handleClick = (): void => {
--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -5,12 +5,11 @@ import {productsItf} from '../interfaces/ProductInterface';
 
 export const  ProductsProvider: React.FC  = ({ children }) => {
   const [productList, setProductList] = useState<productsItf[]>(DEFAULT_VALUE.productList);
+  console.log(productList)
 
   const setProducts = (pdt: productsItf) => {
     setProductList((productList) => [...productList, pdt])
   }
-
-  // const obj = {products, setProducts}
 
   return (
     <ProductsContext.Provider value={{productList, setProducts}}  >
