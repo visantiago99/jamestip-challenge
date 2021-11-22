e189adfd56fcdbc6e42f14280fdba16c3a0f3004
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-11-22 14:50:23 -0300

    LocalStorage done

--- src/pages/ProductsList.tsx
+++ src/pages/ProductsList.tsx
@@ -1,17 +1,20 @@
 import React, { useContext } from 'react';
 import ProductsContext from '../context/ProductsContext';
 import { Link } from 'react-router-dom';
-
 import ProductCard from '../components/ProductCard';
 
 export default function ProductsList() {
   const { productList } = useContext(ProductsContext)
+
   
+  const localObj = JSON.parse(localStorage.getItem('productList') || "")
+  console.log(localObj)
+   
   
   return (
     <div>
       <ul>
-        {productList.map((pdt, i) =><ProductCard {...pdt} index={i} />)}
+        {productList.length > 0 ? productList.map((pdt, i) =><ProductCard {...pdt} index={i} />) : localObj.map((pdt: any, i: any) =><ProductCard {...pdt} index={i} />)}
       </ul>
       <br />
       <br />
